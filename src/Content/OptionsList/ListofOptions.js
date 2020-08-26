363597023434dd6226071e88e278557550530c21
Sean-McInturff <67393034+Sean-McInturff@users.noreply.github.com>
2020-08-26 13:03:36 -0400

    Add files via upload

---
+++ src/Content/OptionsList/ListofOptions.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Options from './Options';
+
+
+export default class ListOfOptions extends Component {
+
+    render() {
+        return (
+            <form className="main__form">
+            <h2>Customize your laptop</h2>
+            <Options
+                features={this.props.features}
+                selected={this.props.selected}
+            />
+          </form>
+        )
+    }
+}
